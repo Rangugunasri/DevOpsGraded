742a036d5c4684b1124eaeae46e5a4ac0e180aaf
student <rangu.sri@hcl.com>
2021-04-20 10:37:47 +0000

    Intial Commit

---
+++ DevopsGraded/src/test/java/com/gunasri/graded/DevopsGraded/DevopsGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunasri.graded.DevopsGraded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
